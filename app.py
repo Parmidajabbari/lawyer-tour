c5ad8d2a9dafb4407423d6fad75f2ffb7ec922df
Parmida Jabari <70059560+Parmidajabbari@users.noreply.github.com>
2025-07-04 16:52:38 +0330

    Update app.py

--- app.py
+++ app.py
@@ -39,4 +39,7 @@ def file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
